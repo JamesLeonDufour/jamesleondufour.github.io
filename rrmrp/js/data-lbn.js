b1b0e11802a56aed2d4fc88f8b19de89a0310bd1
James Léon-Dufour <jamesleondufour@gmail.com>
2015-10-15 11:26:41 +0300

    RRMRP
    RRMRP

---
+++ rrmrp/js/data-lbn.js
@@ -0,0 +1,31 @@
+var rcode2reg = {
+	'1':'Aakar',
+	'2':'Aaley',
+	'3':'Baabda',
+	'4':'Baalbek',
+	'5':'Batroun',
+	'6':'Bcharre',
+	'7':'Beirut',
+	'8':'Bent Jbayl',
+	'9':'Chouf',
+	'10':'El Nabatieh',
+	'11':'Hasbaiya',
+	'12':'Hermel',
+	'13':'Jbayl',
+	'14':'Jezzine',
+	'15':'Kesrouane',
+	'16':'Koura',
+	'17':'Marjaayoun',
+	'18':'Metn',
+	'19':'Miniyeh-Danniyeh',
+	'20':'Rachaiya',
+	'21':'Saida',
+	'22':'Sour',
+	'23':'Trablous',
+	'24':'West Bekaa',
+	'25':'Zahleh',
+	'26':'Zgharta'}
+	
+
+
+
